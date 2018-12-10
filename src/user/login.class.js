1953dca60495eb908a932880016cc9a452a3c5b5
Olivia <olivia.verove@gmail.com>
2018-12-10 16:11:53 +0100

    Effacement des données saisies

--- src/user/login.class.js
+++ src/user/login.class.js
@@ -6,24 +6,30 @@ class Login {
         // Modifier le titre de la page
         $('#main-title').html('Identifiez-vous');
 
+        //Définition des attributs
+        this.login = $('[name="loginField"]');
+        this.password = $('[name="passwordField"]');
+
         //Définition du listener sur le formulaire
         this.formListener();
+        this.submitListener();
     }
 
-/** formListener : gestionnaire d'événements sur le formulaire de login
- * @param void (n'accepte pas de parametres)
- * @return void (ne retourne rien)
- * 
-*/ 
+    /** formListener : gestionnaire d'événements sur le formulaire de login
+     * @param void (n'accepte pas de parametres)
+     * @return void (ne retourne rien)
+     * 
+    */
 
-formListener(){
-    $('#loginForm').on(
-        'keyup',
-        // Callback: fonction appelée si l'événement défini survient
-        function(event) {
-                //vérifier le contenu des deux champs
-                const login = $('[name="loginField"]');
-                const password = $('[name="passwordField"]');
+    formListener() {
+
+        let login = this.login;
+        let password = this.password;
+
+        $('#loginForm').on(
+            'keyup',
+            // Callback: fonction appelée si l'événement défini survient
+            function (event) {
 
                 //Est-ce que les deux champs sont remplis
                 //Et longueur login > 5 caractères
@@ -31,17 +37,45 @@ formListener(){
                 //qu'il doit etre supérieur à 5 caractères
 
                 if (
-                   // login.val() !== '' &&
+                    // login.val() !== '' &&
                     password.val() !== '' &&
-                    login.val().length >= 5){
+                    login.val().length >= 5) {
                     // On peut activer le bouton
                     $('#btnLogin').removeAttr('disabled');
-                }else{
+                } else {
                     $('#btnLogin').attr('disabled', 'disabled');
                 }
-                
-        }
-    );
-}
 
+            }
+        );
+    }
+
+    submitListener() {
+
+        let login = this.login;
+        let password = this.password;
+
+        $('#loginForm').on(
+            'submit',
+            function (event) {
+                event.preventDefault(); //Empêche l'action par défaut
+
+                //Instancie un nouvel utilisateur
+                const user = new User();
+                // Définit le login et le password de l'utilisateur
+                user.setUserName(login.val());
+                user.setPassword(password.val());
+
+                // Gère l'authentification: dit s'il est VRAI ou FAUX
+                if (user.authenticate()) {
+                    console.log('Ok, tu peux y aller');
+                } else {
+                    console.log('KO, t\'as pas le droit !');
+                    login.val();
+                    password.val();
+                    $('#btnLogin').attr('disabled', 'disabled');
+                }
+            }
+        );
+    }
 }
---
+++ src/user/user.class.js
@@ -0,0 +1,39 @@
+/**
+ * @name User
+ * @desc Service de gestion des utilisateurs
+ * @author Aélion
+ * @version 1.0.0
+ */
+
+class User {
+    constructor() { }
+
+    /** Définit le username de l'utilisateur
+    *@param{*} userName
+    */
+    setUserName(userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Définit le mot de passe utilisateur
+     * @param {*} password
+     */
+
+    setPassword(password) {
+        this.password = password;
+    }
+
+/**
+ * Identifie l'utilisateur à partir d'un login et d'un mot de passe
+ * @return boolean
+ */
+
+    authenticate() {
+        if (this.userName === 'oliviaverove' && this.password === 'ov') {
+            this.group = 'Administrateur';
+            return true;
+        }
+        return false;
+    }
+}
